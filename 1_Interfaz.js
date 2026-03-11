4a0b49ae734bac13fc7067d34ef464fe05608166
Pollo Lopez Albini <pollolopeza@gmail.com>
2026-03-11 11:24:48 -0300

    se crea 12_API.js.html y 13_script.js.html

--- 1_Interfaz.js
+++ 1_Interfaz.js
@@ -2,25 +2,25 @@
 /* eslint-env browser */
 /* global google, M */
 function doGet() {
-    return HtmlService.createTemplateFromFile('Panel') 
-        .evaluate()
-        .setTitle('App Sec. Seg. Mercedes')
-        .addMetaTag('viewport', 'width=device-width, initial-scale=1')
-        .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return HtmlService.createTemplateFromFile('Panel')
+    .evaluate()
+    .setTitle('App Sec. Seg. Mercedes')
+    .addMetaTag('viewport', 'width=device-width, initial-scale=1')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 
 function abrirPanelTablerosModal() {
-    try {
-        registrarAccesoUsuario("Panel Principal SPA");
-        const html = HtmlService.createTemplateFromFile("Panel") 
-            .evaluate()
-            .setWidth(1000)
-            .setHeight(750);
-        SpreadsheetApp.getUi().showModalDialog(html, "Gestión de Sec. Seg. MM.");
-    } catch (e) {
-        const ui = SpreadsheetApp.getUi();
-        ui.alert('Error', `No se pudo cargar Panel.html: ${e.message}`, ui.ButtonSet.OK);
-    }
+  try {
+    registrarAccesoUsuario("Panel Principal SPA");
+    const html = HtmlService.createTemplateFromFile("Panel")
+      .evaluate()
+      .setWidth(1000)
+      .setHeight(750);
+    SpreadsheetApp.getUi().showModalDialog(html, "Gestión de Sec. Seg. MM.");
+  } catch (e) {
+    const ui = SpreadsheetApp.getUi();
+    ui.alert('Error', `No se pudo cargar Panel.html: ${e.message}`, ui.ButtonSet.OK);
+  }
 }
 /** * Función necesaria para insertar archivos HTML dentro de otros (CSS/JS) */
 function incluir(nombreArchivo) {
@@ -32,7 +32,7 @@ function getUsuarioInfo() {
   let info = {
     email: email,
     nombre: "Usuario Desconocido",
-    rol: "VISOR" 
+    rol: "VISOR"
   };
 
   try {
@@ -41,8 +41,8 @@ function getUsuarioInfo() {
 
     for (let i = 1; i < datos.length; i++) {
       if (datos[i][0].toString().toLowerCase() === email) {
-        info.nombre = datos[i][1]; 
-        info.rol = datos[i][2].toString().toUpperCase(); 
+        info.nombre = datos[i][1];
+        info.rol = datos[i][2].toString().toUpperCase();
         break;
       }
     }
@@ -51,3 +51,5 @@ function getUsuarioInfo() {
   }
   return info;
 }
+
+
